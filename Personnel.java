586adf49f7534993b7833d17a44df197a92305d8
xiachifeng <72387137+Veneto-xia@users.noreply.github.com>
2020-10-25 20:48:22 +0800

    Add files via upload

---
+++ Personnel.java
@@ -0,0 +1,14 @@
+package 第二次上机;
+
+public class Personnel {
+	int number;
+    String name;
+    char sex;
+    Course a;
+    public Personnel(int number, String name, char sex) {
+        this.number = number;
+        this.name = name;
+        this.sex = sex;
+
+    }
+}
---
+++ Student.java
@@ -0,0 +1,12 @@
+package 第二次上机;
+
+public class Student extends Personnel {
+    public Student(int number, String name, char sex,Course a){
+        super(number,name,sex);
+        this.a = a;
+    }
+    public String toString(){
+        return  "\n" + "Student(学生信息):  姓名：" + name
+                + "    学号：" + number + "   性别：" + sex + "\n" + "所选课程信息:   " + a;
+    }
+    }
